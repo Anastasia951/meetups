bcccde79bf38f02855cfbc4ad897a38ad0a33fd8
Anastasia951 <n.ivanova504@gmail.com>
2022-07-17 22:59:59 +0300

    react-hook-form

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,20 +1,41 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { ILogin } from '../../constants/forms'
 import { routes } from '../../constants/routes'
 
 import './Login.scss'
 
 const Login = () => {
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILogin>()
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={onSubmit}>
       <h2 className='form__title'>Вход</h2>
       <div className='field'>
         <label>Email</label>
-        <input type='email' placeholder='mail@mail.ru' />
+        <input
+          {...register('email', { required: true })}
+          type='email'
+          placeholder='mail@mail.ru'
+        />
       </div>
       <div className='field'>
         <label>Пароль</label>
-        <input className='password' type='password' />
+        <input
+          {...register('password', { required: true })}
+          className='password'
+          type='password'
+        />
       </div>
 
       <button type='submit' className='form__submit'>
--- frontend/src/pages/Register/Register.tsx
+++ frontend/src/pages/Register/Register.tsx
@@ -1,26 +1,49 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
+import { IRegister } from '../../constants/forms'
 import { routes } from '../../constants/routes'
 
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IRegister>()
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
   return (
-    <form className='form'>
+    <form className='form' onSubmit={onSubmit}>
       <h2 className='form__title'>Регистрация</h2>
       <div className='field'>
         <label>Email</label>
-        <input type='email' placeholder='mail@mail.ru' />
+        <input
+          {...register('email', { required: true })}
+          type='email'
+          placeholder='mail@mail.ru'
+        />
       </div>
       <div className='field'>
         <label>Имя</label>
-        <input type='text' />
+        <input type='text' {...register('name', { required: true, min: 3 })} />
       </div>
       <div className='field'>
         <label>Пароль</label>
-        <input className='password' type='password' />
+        <input
+          {...register('password', { required: true })}
+          className='password'
+          type='password'
+        />
       </div>
       <div className='field'>
         <label>Повтор пароля</label>
-        <input className='password' type='password' />
+        <input
+          {...register('repeatPassword', { required: true })}
+          className='password'
+          type='password'
+        />
       </div>
 
       <button type='submit' className='form__submit'>
